cda7cb7df86ac9b45ce138e8eee1c48fbb8db300
Jire <thomasgnappo@gmail.com>
2023-06-26 23:04:32 -0400

    Increased default heap size for server to run smoother

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,10 +63,9 @@ tasks.withType<JavaCompile>().configureEach {
 }
 
 val defaultJvmArgs = arrayOf(
-    //"-XX:+UseZGC",
-    "-Xmx4g",
-    "-Xms2g",
     "-XX:-OmitStackTraceInFastThrow",
+    //"-XX:+UseZGC",
+    "-Xmx16g", "-Xms8g",
     /*    "--add-opens=java.base/java.time=ALL-UNNAMED",
         "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
         "--add-opens=java.base/java.lang=ALL-UNNAMED",
